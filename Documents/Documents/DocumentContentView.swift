1ee1717f9cbc177999b69717fc8b54da8c504ac7
jay.choi <jay.choi@kakaocorp.com>
2022-02-26 16:36:37 +0900

    add actions to DocumentContentView

--- Documents/Documents/DocumentContentView.swift
+++ Documents/Documents/DocumentContentView.swift
@@ -5,42 +5,67 @@
 //  Created by oozoofrog on 2022/02/26.
 //
 
+import Combine
 import UIKit
 import SwiftUI
 import SwiftLayout
+import VisionKit
 
 class DocumentContentView: UIView, LayoutBuilding {
-
-    var deactivable: Deactivable?
     
-    lazy var backgroundView = UIView()
-    
-    var layout: some Layout {
-        self {
-            backgroundView.config({ backgroundView in
-                backgroundView.backgroundColor = .yellow
-            }).anchors {
-                Anchors.allSides()
-            }
-            UILabel().config { label in
-                label.text = "Document"
-                label.textColor = .darkText
-            }.anchors {
-                Anchors(.centerX, .centerY)
-            }
-        }
+    var requestDocumentAction: AnyPublisher<Void, Never> {
+        requestDocumentClickSubject.eraseToAnyPublisher()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        updateLayout()
+        initViews()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        initViews()
+    }
+    
+    private func initViews() {
         updateLayout()
     }
-
+    
+    private let requestDocumentClickSubject = PassthroughSubject<Void, Never>()
+    
+    var deactivable: Deactivable?
+    
+    private lazy var backgroundView = UIView()
+    private lazy var toggleButton: UIButton = {
+        let image = UIImage(systemName: "plus",
+                            withConfiguration: UIImage.SymbolConfiguration(font: .boldSystemFont(ofSize: 40.0)))
+        let toggleAction = UIAction(title: "", image: image) { [weak self] _ in
+            self?.requestDocumentClickSubject.send()
+        }
+        let button = UIButton(primaryAction: toggleAction)
+        button.tintColor = .black
+        return button
+    }()
+    
+    var layout: some Layout {
+        self {
+            backgroundView.anchors {
+                Anchors.allSides()
+            }
+            if VNDocumentCameraViewController.isSupported {
+                toggleButton.anchors {
+                    Anchors.allSides()
+                }
+            } else {
+                UILabel().config { label in
+                    label.text = "unable capture document"
+                }.anchors {
+                    Anchors(.centerX, .centerY)
+                }
+            }
+        }
+    }
+    
 }
 
 struct DocumentContentView_Preview: PreviewProvider, UIViewRepresentable {
