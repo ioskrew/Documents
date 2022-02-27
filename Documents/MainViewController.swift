3c9ee5214339dad93228423e812e465cccc7fdfd
jay.choi <jay.choi@kakaocorp.com>
2022-02-27 17:21:44 +0900

    show animation in layout updating

--- Documents/MainViewController.swift
+++ Documents/MainViewController.swift
@@ -14,6 +14,7 @@ final class MainViewController: UIViewController, LayoutBuilding {
     
     var deactivable: Deactivable?
     
+    lazy var sampleView = SampleView()
     lazy var contentView = DocumentContentView()
     lazy var scroller = DocumentScroller()
     lazy var memoView = DocumentMemoView()
@@ -23,34 +24,37 @@ final class MainViewController: UIViewController, LayoutBuilding {
     
     var layout: some Layout {
         view {
-            contentView.identifying("document").anchors {
+            sampleView.anchors {
                 Anchors.cap()
-                Anchors(.height).equalTo(view).setMultiplier(contentMultiplier)
             }
-            scroller.setAnimationHandler({ [weak self] view in
-                guard let self = self else { return }
-                view.alpha = self.hideBottom ? 0.0 : 1.0
-            }).identifying("scroller").anchors {
-                if hideBottom {
-                    Anchors(.top).equalTo(view.bottomAnchor)
-                } else {
-                    Anchors(.top).equalTo("document", attribute: .bottom)
-                }
-                Anchors.horizontal()
-                Anchors(.height).equalTo(constant: hideBottom ? 0.0 : 44.0)
-            }
-            memoView.setAnimationHandler({ [weak self] view in
-                guard let self = self else { return }
-                view.alpha = self.hideBottom ? 0.0 : 1.0
-            }).anchors {
-                if hideBottom {
-                    Anchors(.top).equalTo(view.bottomAnchor)
-                    Anchors.horizontal()
-                } else {
-                    Anchors(.top).equalTo("scroller", attribute: .bottom)
-                    Anchors.shoe()
-                }
-            }
+//            contentView.identifying("document").anchors {
+//                Anchors.cap()
+//                Anchors(.height).equalTo(view).setMultiplier(contentMultiplier)
+//            }
+//            scroller.setAnimationHandler({ [weak self] view in
+//                guard let self = self else { return }
+//                view.alpha = self.hideBottom ? 0.0 : 1.0
+//            }).identifying("scroller").anchors {
+//                if hideBottom {
+//                    Anchors(.top).equalTo(view.bottomAnchor)
+//                } else {
+//                    Anchors(.top).equalTo("document", attribute: .bottom)
+//                }
+//                Anchors.horizontal()
+//                Anchors(.height).equalTo(constant: hideBottom ? 0.0 : 44.0)
+//            }
+//            memoView.setAnimationHandler({ [weak self] view in
+//                guard let self = self else { return }
+//                view.alpha = self.hideBottom ? 0.0 : 1.0
+//            }).anchors {
+//                if hideBottom {
+//                    Anchors(.top).equalTo(view.bottomAnchor)
+//                    Anchors.horizontal()
+//                } else {
+//                    Anchors(.top).equalTo("scroller", attribute: .bottom)
+//                    Anchors.shoe()
+//                }
+//            }
         }
     }
     
@@ -95,15 +99,107 @@ extension MainViewController: FakeDocumentCameraViewDelegate {
     }
 }
 
-struct ContentView: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) -> some UIViewController {
-        MainViewController(nibName: nil, bundle: nil)
+final class SampleView: UIView, LayoutBuilding {
+    enum Show {
+        case showYellow
+        case showBlack
+        case showAll
+    }
+    var deactivable: Deactivable?
+    lazy var yellow = UIView()
+    lazy var black = UIView()
+    var show: Show = .showAll {
+        didSet {
+            updateLayout(animated: true)
+        }
     }
     
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    @LayoutBuilder
+    var layout: some Layout {
+        self {
+            switch show {
+            case .showYellow:
+                yellow.anchors {
+                    Anchors.allSides()
+                }
+                black.setAnimationHandler({ view in
+                    view.alpha = 0.0
+                }).anchors {
+                    Anchors(.height).equalTo(constant: 0.0)
+                    Anchors.shoe()
+                }
+            case .showBlack:
+                yellow.setAnimationHandler({ view in
+                    view.alpha = 0.0
+                }).anchors {
+                    Anchors(.height).equalTo(constant: 0.0)
+                    Anchors.cap()
+                }
+                black.anchors {
+                    Anchors.allSides()
+                }
+            case .showAll:
+                yellow.setAnimationHandler({ view in
+                    view.alpha = 1.0
+                }).anchors {
+                    Anchors.cap()
+                    Anchors(.bottom).equalTo(black, attribute: .top)
+                    Anchors(.height).equalTo(black)
+                }
+                black.setAnimationHandler({ view in
+                    view.alpha = 1.0
+                }).anchors {
+                    Anchors.shoe()
+                }
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func initViews() {
+        yellow.backgroundColor = .yellow
+        black.backgroundColor = .darkGray
+        updateLayout(.automaticIdentifierAssignment)
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(toggle)))
+    }
+    
+    @objc
+    func toggle() {
+        switch show {
+        case .showYellow:
+            self.show = .showAll
+        case .showBlack:
+            self.show = .showAll
+        case .showAll:
+            self.show = Bool.random() ? .showYellow : .showBlack
+        }
+    }
+    
+}
+
+struct ContentView: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        SampleView()
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
         
     }
+//    func makeUIViewController(context: Context) -> some UIViewController {
+//        MainViewController(nibName: nil, bundle: nil)
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+//
+//    }
     
 }
 
